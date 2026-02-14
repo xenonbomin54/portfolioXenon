5c10a5eda97d20c29393b0d563bb1927808080ad
xenonbomin54 <xenonbomin54@gmail.com>
2026-02-14 20:24:44 +0900

    div박스 내용물 자동설정

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const container = document.getElementById('cardBox');
 
-let divBox = `
-        <div class="divs" style="background-color:white; width: calc(200px + 15vw + 4vh); height: calc(150px + 15vw + 4vh); max-width: 90%; margin:0 auto; margin-top:10vh; border-radius: calc( 1vw + 5px);">
-            hello!
+let i=0;
+let gaesoo=2;
+
+for (i=0;i<gaesoo;i++){
+    switch(i){
+        case 0:
+            var boxValue = "memoUSB"
+            break;
+        case 1:
+            var boxValue = "portfolio page"
+            break;
+    }
+    let divBox = `
+        <div class="divs" style="background-color:white; width: calc(200px + 15vw + 4vh); height: calc(150px + 15vw + 4vh); max-width: 90%; margin:0 auto; margin-top:10vh; border-radius: calc( 1vw + 5px); text-align:center;">
+            <div id="${i}">${boxValue}</div>
         </div>
     `;
     container.insertAdjacentHTML('beforeend', divBox);
+}
