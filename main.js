7c85e2f71b1f2a3386f459705bd1cffbc0823dea
xenonbomin54 <xenonbomin54@gmail.com>
2026-02-18 22:17:36 +0900

    박스 내용물 폰트 유동적 설정

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (i=0;i<gaesoo;i++){
             break;
     }
     let divBox = `
-        <div class="divs" style="background-color:white; width: calc(200px + 15vw + 4vh); height: calc(150px + 15vw + 4vh); max-width: 90%; margin:0 auto; margin-top:10vh; border-radius: calc( 1vw + 5px); text-align:center;">
-            <div id="bv">${boxValue}</div>
-            <div id="bv2">${boxValue2}</div>
+        <div class = "divs">
+            <div class="bv">${boxValue}</div>
+            <div class="bv2">${boxValue2}</div>
         </div>
     `;
     container.insertAdjacentHTML('beforeend', divBox);
